d175221431e3602befde90f4dada99303e7d979a
Adithyarao19 <adikudige619gmail.com>
2025-07-13 22:39:54 +0530

    image tag error

--- modules/core-infra/variables.tf
+++ modules/core-infra/variables.tf
@@ -2,4 +2,7 @@ variable "region" { default = "us-east-1" }
 variable "vpc_cidr" { default = "10.0.0.0/16" }
 variable "db_name" { default = "medusa" }
 variable "db_username" { default = "admin" }
-variable "db_password" { type = string, sensitive = true }
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
